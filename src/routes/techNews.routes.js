3ea07f7f25e8ab083c79ea083e753cf96f33152e
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-06-20 22:42:17 +0530

    Added logic for routing Tech News API

--- src/routes/techNews.routes.js
+++ src/routes/techNews.routes.js
@@ -1,1 +1,8 @@
-import express from 'express'
+import express from 'express';
+import { getTechNews } from '../controllers/techNews.controllers.js';
+
+const newsRouter = express.Router();
+
+newsRouter.get('/api', getTechNews);
+
+export default newsRouter;
