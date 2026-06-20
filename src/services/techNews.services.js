f4a97ab51fb4dc05d966a6ee56fca69b51fe1bc8
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-06-20 22:40:09 +0530

    Added logics for Tech News API calling

--- src/services/techNews.services.js
+++ src/services/techNews.services.js
@@ -1,7 +1,29 @@
-export const techNewsData = async () => {
-    return [
-        { id: 1, title: 'Next.js' },
-        { id: 1, title: 'Next.js' },
-        { id: 1, title: 'Next.js' },
-    ]
-}
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const fetchTechNews = async () => {
+    try {
+        const apiResponse = await axios.get(`${process.env.TECH_NEWS_URL}`, 
+            {
+                params: {
+                    q: 'technology',
+                    lang: 'en',
+                    max: 5,
+                    apikey: process.env.TECH_NEWS_API_KEY
+                }
+            }
+        )
+
+        return apiResponse.data.articles.map((article, idx) => ({
+            id: idx + 1,
+            title: article.title,
+            description: article.description,
+            content: article.content,
+            url: article.url
+        }))
+    } catch (error) {
+        console.error('Error', error);
+    }
+};
