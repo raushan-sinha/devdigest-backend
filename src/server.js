7b04b229404a580d4450b6f25974f3da6e5c4012
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-06-20 22:46:10 +0530

    Called app file to run Backend workflows

---
+++ src/server.js
@@ -0,0 +1,10 @@
+import app from './app.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const Port = process.env.PORT || 5000;
+
+app.listen(Port, () => {
+    console.log(`Server is running on http://localhost:${Port}`);
+});
