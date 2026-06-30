62f6f3ee5a195d19018e6c1343665bc39ef74525
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-06-30 20:49:05 +0530

    Attached page route of Jobs posting

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import newsRouter from './routes/techNews.routes.js';
+import jobsRouter from './routes/jobsPosting.routes.js';
 
 const app = express();
 app.use(express.json());
@@ -8,5 +9,8 @@ app.use(cors());
 
 // Tech News Router -
 app.use('/tech-news', newsRouter);
+
+// Jobs Router -
+app.use('/jobs', jobsRouter);
 
 export default app;
